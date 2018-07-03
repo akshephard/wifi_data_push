17223913654c0f9e8759686a073e87d0113108e6
[akshephard] <akshephard@ucdavis.edu>
2018-07-03 08:49:04 -0700

    utilized write points better by putting all of the data from one column together in one push

--- wifi_data_push.py
+++ wifi_data_push.py
@@ -13,40 +13,25 @@ import configparser
 import subprocess
 import numpy as np
 from datetime import datetime
+'''
+loop over each columnn in dataframe grab column name first and parse name
+add all points as a list of dictionaries of json
+i.e.
 
-def get_DB_client(host,
-                  username,
-                  password,
-                  database,
-                  port=8086,
-                  ssl=True,
-                  verify_ssl=True):
-
-    client = InfluxDBClient(host=host, port=port, username=username, password=password, database=database,
-                            ssl=ssl, verify_ssl=verify_ssl)
-    return client
-
-
-def post_to_DB(client,data,measurement,tags,fields):
-    for x in range(data.shape[0]): #loop through all rows
-
-        #get timestamp as index and convert it to Unix Epoch Pacific Time
-        timestamp = data.iloc[[x],[1]].axes[0].tolist()[0]
-        d = timestamp.to_pydatetime()
-        pushTime = int(time.mktime(d.timetuple()))
-
-        for y in range(data.shape[1]): #loop through all columns
-            #get value from data frame a particular row x and column y
-            ap_value = float(data.iloc[[x],[y]].values)
-
-            #get column name which the name of the current access point
-            current_ap_name = data.iloc[[x],[y]].dtypes.index[0]
-
-            #parse ap name to map to building information
-            parsed_ap_name = current_ap_name.split('-')
-
-            #create formatted json to push to database
             pushData = [
+                    {
+                        "measurement": measurement,
+                        "tags": {
+                            tags: current_ap_name,
+                            "building_number": 90,
+                            "floor": 2,
+                            "room": 50
+                        },
+                        "time": pushTime,
+                        "fields": {
+                            fields: ap_value
+                        }
+                    },
                     {
                         "measurement": measurement,
                         "tags": {
@@ -61,12 +46,73 @@ def post_to_DB(client,data,measurement,tags,fields):
                         }
                     }
                 ]
-            #check to see if value is NaN before pushing
-            if (not math.isnan(ap_value)):
-                ret = client.write_points(pushData)
-                if(ret == False):
-                    print("Failed to push")
-                    break;
+
+'''
+def get_DB_client(host,
+                  username,
+                  password,
+                  database,
+                  port=8086,
+                  ssl=True,
+                  verify_ssl=True):
+
+    client = InfluxDBClient(host=host, port=port, username=username, password=password, database=database,
+                            ssl=ssl, verify_ssl=verify_ssl)
+    return client
+
+
+def post_to_DB(client,data,measurement,tags,fields):
+    unique_list = []
+    output = set()
+    print(data.head())
+    print(data.stack().head())
+    for colNum in range(data.shape[1]): #loop through all columns
+
+        #get column name which the name of the current access point
+        current_ap_name = data.iloc[[0],[colNum]].dtypes.index[0]
+        print(current_ap_name)
+        #print(data.head())
+        #print(data.stack().head())
+        #print(colNum)
+        #parse ap name to map to building information
+        #parsed_ap_name = current_ap_name.split('-')
+        #print(parsed_ap_name)
+
+        pushData = []
+        for rowNum in range(data.shape[0]): #loop through all rows
+
+            #get value from data frame a particular row x and column y
+            ap_value = float(data.iloc[[rowNum],[colNum]].values)
+
+            #check if value is nan and drop point if it is
+            if (math.isnan(ap_value)):
+                continue
+
+            #get timestamp as index and convert it to Unix Epoch Pacific Time
+            timestamp = data.iloc[[rowNum],[1]].axes[0].tolist()[0]
+            d = timestamp.to_pydatetime()
+            pushTime = int(time.mktime(d.timetuple()))
+
+            #create formatted json to push to database
+            json_dict = {
+                "measurement": measurement,
+                "tags": {
+                            tags: current_ap_name,
+                            "building_number": 90,
+                            "floor": 2,
+                            "room": 50
+                        },
+                "time": pushTime,
+                "fields": {
+                            fields: ap_value
+                        }
+            }
+            pushData.append(json_dict)
+
+        ret = client.write_points(pushData)
+        if(ret == False):
+            print("Failed to push")
+            break;
 
 ######################## MAIN ########################
 
