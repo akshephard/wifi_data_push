90bd4af00fc17d97d5e5fcdfba0ccdb4ace0d16a
[akshephard] <akshephard@ucdavis.edu>
2018-07-16 18:28:05 -0700

    Made full transistion to Influx_Dataframe_Client.py

--- wifi_data_push.py
+++ wifi_data_push.py
@@ -5,6 +5,7 @@ import math
 from influxdb import InfluxDBClient
 from influxdb import DataFrameClient
 from datetime import datetime, timedelta
+from pytz import timezone
 import datetime######################## MAIN ########################
 import pytz
 import hashlib
@@ -14,42 +15,14 @@ import subprocess
 import numpy as np
 from datetime import datetime
 from tabulate import tabulate
+import os
+import sys
+print(sys.path)
+#sys.path.append('.')
+#from ..influx_dataframe_client import Influx_Dataframe_Client
+#import Influx_Dataframe_Client
+from Influx_Dataframe_Client import Influx_Dataframe_Client
 
-'''
-loop over each columnn in dataframe grab column name first and parse name
-add all points as a list of dictionaries of json
-i.e.
-
-            pushData = [
-                    {
-                        "measurement": measurement,
-                        "tags": {
-                            tags: current_ap_name,
-                            "building_number": 90,
-                            "floor": 2,
-                            "room": 50
-                        },
-                        "time": pushTime,
-                        "fields": {
-                            fields: ap_value
-                        }
-                    },
-                    {
-                        "measurement": measurement,
-                        "tags": {
-                            tags: current_ap_name,
-                            "building_number": 90,
-                            "floor": 2,
-                            "room": 50
-                        },
-                        "time": pushTime,
-                        "fields": {
-                            fields: ap_value
-                        }
-                    }
-                ]
-
-'''
 def get_DB_client(host,
                   username,
                   password,
@@ -63,23 +36,46 @@ def get_DB_client(host,
     return client
 
 def time_transform(timestamp):
+
     d = timestamp.to_pydatetime()
-    pushTime = int(time.mktime(d.timetuple()))
-    #print(pushTime)
-    return int(pushTime)
+    #print(d.strftime('%Y-%m-%dT%H:%M:%SZ'))
+    localtz = timezone('US/Pacific')
+    d = localtz.localize(d)
+    d = d.astimezone(timezone('UTC'))
+    return d
 
-'''def transform_to_dict(s, key):
-    dic = {}
-    dic[key] = s
-    return dic
-'''
-def transform_to_dict(s, tags):
 
-    dic = {}
-    for tag in tags:
-        dic[tag] = s[tag]
+def parse_ap(s):
+    return s.split('-')
 
-    return dic
+def get_building(s):
+    return s[1]
+
+def get_floor(s):
+    x=0
+    if (len(s[2]) > 3):
+        while(len(s[2]) > x and s[2][x].isalpha()):
+            if(x == (len(s[2]) -1)):
+                return 99
+            else:
+                x += 1
+        return s[2][x]
+    else:
+        return 1
+
+def get_room(s):
+    x=0
+    if (len(s[2]) > 3):
+        while(len(s[2]) > x and s[2][x].isalpha()):
+            if(x == (len(s[2]) -1)):
+                return 99
+            else:
+                x += 1
+        x += 1
+        return s[2][x:]
+    else:
+        return s[2]
+
 
 def post_to_DB(client,json):
     ret = client.write_points(json,batch_size=16384)
@@ -87,28 +83,23 @@ def post_to_DB(client,json):
 
 
 def build_json(data, tags, fields, measurement):
-    print(data.head())
-    print(data.dtypes)
 
     data['measurement'] = measurement
-    print(data.head())
+
     #data["fields"] = data.iloc[:,2].apply(transform_to_dict, tags=fields)
     data["tags"] = data.apply(transform_to_dict, tags=tags, axis=1)
     data["fields"] = data.apply(transform_to_dict, tags=fields, axis=1)
     #data["tags"] = data.iloc[:,1].apply(transform_to_dict, tags=tags)
-    print(data.dtypes)
-    print(data.head())
 
     #build a list of dictionaries containing json data to give to client
     #only take relevant columns from dataframe
-    json = data[["measurement","time", "tags", "fields"]].head().to_dict("records")
-    print(json)
-
+    json = data[["measurement","time", "tags", "fields"]].to_dict("records")
+    print(data.head())
     return json
 
 ######################## MAIN ########################
 
-def main(conf_file="wifi_local_config.ini"):
+def main(conf_file="/home/werd/lbnl_summer/wifi_data_push/wifi_scraping_config.ini"):
     # read arguments passed at .py file call
     parser = argparse.ArgumentParser()
     parser.add_argument("pickle", help="pickle file")
@@ -116,6 +107,8 @@ def main(conf_file="wifi_local_config.ini"):
     args = parser.parse_args()
     pickle_file = args.pickle
 
+    test_client = Influx_Dataframe_Client('/home/werd/lbnl_summer/wifi_data_push/local_server.ini')
+    test_client.make_client()
 
     # read from config file
     conf_file = conf_file
@@ -136,44 +129,36 @@ def main(conf_file="wifi_local_config.ini"):
 
 
     # get and summarize wifi data
-    data = pd.read_pickle(pickle_file)
-
+    whole_data = pd.read_pickle(pickle_file)
+    data = whole_data.head()
     #get dataframe in right format before creating json
     #puts all of the columns associated with a timestamp together
     data = data.stack()
     data = data.reset_index()
 
-    #change time in correct format
-    #time_transform changes from x to Unix Epoch time
-    #time can also be in x format, make sure that final datatype is string or int
+    #change time to correct format and put into UTC from PST
+
+    print(data.head())
     data.iloc[:,0] = data.iloc[:,0].apply(time_transform)
 
     #rename columns so that the proper keys will appear in the json list
     #containing all the individual measurement, also make sure time is int not float
-    print(data.head())
     data.columns = ['time', 'ap_name', 'AP_count']
-    data['building_number'] = 90
-    data['floor'] = 2
-    data['room'] = 43
-    data['time'] = data['time'].astype(int)
+    print(data.head())
+    #make new column with split up AP name
+    data['parse_ap_name'] = data.iloc[:,1].apply(parse_ap)
+    #use split AP name to get building, floor and room number
+    data['building_number'] = data['parse_ap_name'].apply(get_building)
+    data['floor'] = data['parse_ap_name'].apply(get_floor)
+    data['room'] = data['parse_ap_name'].apply(get_room)
+
     print(data.head())
     #remove all of the rows which have NaN
     #optional depending on if your data contains NaN
     data.dropna(inplace=True)
 
-    # create client
-    client =get_DB_client(host=host,
-                          username=username,
-                          password=password,
-                          database=database,
-                          port=port,
-                          ssl=False,
-                          verify_ssl=True)
-    #create json dictionary list to give to the client
-    json = build_json(data,tags,fields,measurement)
+    test_client.write_data(data,tags,fields,measurement,database='new_wifi_data')
 
-    #post to db
-    ret = post_to_DB(client,json)
     return
 
 if __name__ == "__main__":
